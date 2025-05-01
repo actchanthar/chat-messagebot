1eb0ab9bcfc2e2d5f15cce5986040a4b71c2a9e4
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-05-01 15:10:56 +0630

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 BOT_TOKEN = "7784918819:AAGxcb10Je-oSKZVoOGDjpcpaFgMq1FNTr8"
-MONGO_URI = "mongodb+srv://chatbotadmin:yourpassword123@cluster0.mongodb.net/?retryWrites=true&w=majority"
-REQUIRED_CHANNELS = [-100123456789, -100987654321]  # Update with your channel IDs
+MONGO_URI = "mongodb+srv://2234act:2234act@cluster0.rwjacbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+REQUIRED_CHANNELS = [-1002097823468, -1001610001670]  # Force-sub channels
 PER_MESSAGE_REWARD = 1
-MIN_WITHDRAWAL = 100
+MIN_WITHDRAWAL = 500
 SPAM_THRESHOLD = 5
 SIMILARITY_THRESHOLD = 0.8
-LOG_CHANNEL = -100123456789  # Update with your log channel ID
-ADMIN_IDS = [123456789]  # Update with your super admin ID
+LOG_CHANNEL = -1002185995735  # Log channel
+ADMIN_IDS = [5062124930]  # Replace with your Telegram user ID
