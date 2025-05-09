d1245a0f5c0eb235c5f713c6fa99e3769a559f04
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-05-09 18:49:04 +0630

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 # Bot token from environment variable
-BOT_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+BOT_TOKEN = os.environ.get("7784918819:AAGxcb10Je-oSKZVoOGDjpcpaFgMq1FNTr8")
 
 # MongoDB URI from environment variable
-MONGODB_URI = os.environ.get("MONGODB_URI")
+MONGODB_URI = os.environ.get("mongodb+srv://2234act:2234act@cluster0.rwjacbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # Admin user IDs (list of strings)
-ADMIN_IDS = os.environ.get("ADMIN_IDS", "").split(",")
+ADMIN_IDS = os.environ.get("ADMIN_IDS", "5062124930").split(",")
 
 # Spam detection settings
 SPAM_THRESHOLD = 5  # Number of similar messages allowed within time window
