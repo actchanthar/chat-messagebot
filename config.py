00481b639c8d7b0c71191c86ea1d67dafbefa580
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-05-10 18:25:53 +0630

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ CHANNEL_ID = os.environ.get("CHANNEL_ID")
 CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME")
 ADMIN_IDS = os.environ.get("ADMIN_IDS", "").split(",")
 CURRENCY = "kyat"
+WITHDRAWAL_THRESHOLD = 100  # Minimum balance for withdrawal
