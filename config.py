cdb577a232057960bd447dd2bd22764b3a84b68a
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-05-14 18:03:11 +0630

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 # config.py
 # Bot token
-BOT_TOKEN = "7784918819:AAHS_tdSRck51UlgW_RQZ1LMSsXrLzqD7Oo"  # Replace with your actual bot token
+BOT_TOKEN = "7784918819:AAHS_tdSRck51UlgW_RQZ1LMSsXrLzqD7Oo"
 
 # MongoDB settings
-MONGODB_NAME = "actchat1"  # Replace with your MongoDB database name
+MONGODB_NAME = "actchat1"
 MONGODB_URL = "mongodb+srv://2234act:2234act@cluster0.rwjacbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Withdrawal settings
-WITHDRAWAL_THRESHOLD = 100  # Minimum withdrawal amount
-DAILY_WITHDRAWAL_LIMIT = 1000  # Daily withdrawal limit
+WITHDRAWAL_THRESHOLD = 100
+DAILY_WITHDRAWAL_LIMIT = 1000
 CURRENCY = "kyat"
 
-# Group chat ID for announcements
-GROUP_CHAT_ID = "-1002061898677"  # Replace with your actual group chat ID if different
+# Group chat IDs for announcements and message counting
+GROUP_CHAT_IDS = ["-1002061898677", "-1002217915135"]
 
 # Log channel ID for withdrawal requests
-LOG_CHANNEL_ID = "-1002555129360"  # Replace with your actual log channel ID if different
+LOG_CHANNEL_ID = "-1002555129360"
 
 # Payment methods
 PAYMENT_METHODS = ["KBZ Pay", "Wave Pay", "Phone Bill"]
