c8bf77ae517e1485dc83fa00f2d7f84acfb01c14
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-05-14 17:56:20 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
 from telegram.ext import Application
-from plugins import start, withdrawal, balance, top, help, message_handler, broadcast  # Add broadcast
+from plugins import start, withdrawal, balance, top, help, message_handler, broadcast, users  # Add users
 from config import BOT_TOKEN as TOKEN
 
 def main():
@@ -11,7 +11,8 @@ def main():
     top.register_handlers(application)
     help.register_handlers(application)
     message_handler.register_handlers(application)
-    broadcast.register_handlers(application)  # Register the broadcast plugin
+    broadcast.register_handlers(application)
+    users.register_handlers(application)  # Register the users plugin
     application.run_polling()
 
 if __name__ == "__main__":
