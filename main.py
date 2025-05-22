11be6e03dff5ec242285fdb67ced2cca5da8df05
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-05-22 17:26:08 +0630

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from plugins.test_count import register_handlers as test_count_handlers
 from plugins.rmamount import register_handlers as rmamount_handlers
 from plugins.check import register_handlers as check_handlers
 from plugins.add_bonus import register_handlers as add_bonus_handlers
-from plugins.grok import register_handlers as grok_handlers  # Add this
+from plugins.grok import register_handlers as grok_handlers  # If added earlier
 import logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -63,7 +63,7 @@ def main():
     rmamount_handlers(application)
     check_handlers(application)
     add_bonus_handlers(application)
-    grok_handlers(application)  # Add this
+    # grok_handlers(application)  # Uncomment if you want /grok
 
     try:
         application.run_polling(
