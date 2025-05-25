bcecdc77dd5348737382357340665344e7964e81
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-05-25 16:11:46 +0630

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-BOT_TOKEN = "7784918819:AAHS_tdSRck51UlgW_RQZ1LMSsXrLzqD7Oo"  # REPLACE
+BOT_TOKEN = "7784918819:AAHS_tdSRck51UlgW_RQZ1LMSsXrLzqD7Oo"
 BOT_USERNAME = "@actearnbot"
 MONGODB_NAME = "actchat1"
-MONGODB_URL = "mongodb+srv://2234act:2234act@cluster0.rwjacbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # REPLACE
+MONGODB_URL = "mongodb+srv://2234act:2234act@cluster0.rwjacbj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 WITHDRAWAL_THRESHOLD = 100
 DAILY_WITHDRAWAL_LIMIT = 2500
 CURRENCY = "kyat"
