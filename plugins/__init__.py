c7f5b86459f5465b5dd8138df31d2381ab43d65b
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-05-27 17:19:21 +0630

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,1 +1,17 @@
-# Empty file
+# Import all plugin modules to make them available via 'from plugins import ...'
+from .addgroup import *
+from .admin import *
+from .balance import *
+from .broadcast import *
+from .channel import *
+from .checkgroup import *
+from .couple import *
+from .help import *
+from .message_handler import *
+from .setphonebill import *
+from .start import *
+from .top import *
+from .transfer import *
+from .users import *
+from .withdrawal import *
+from .rmamount import *  # Add rmamount to the package
