25247b5ed9b2af0646118ca83c7fae98de495cbc
actchanthar <159926023+actchanthar@users.noreply.github.com>
2025-09-21 10:52:21 +0630

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,11 +1,1 @@
-# Plugins package initialization
-# Import all plugin modules to make them available
-from . import message_handler
-from . import balance
-from . import admin
-from . import broadcast
-from . import withdrawal
-from . import stats
-from . import help
-
-__all__ = ['message_handler', 'balance', 'admin', 'broadcast', 'withdrawal', 'stats', 'help']
+#
